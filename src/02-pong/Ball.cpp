4e728f1e1af47cd87bd5e32faad776aaa30f7e7a
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2024-05-04 19:55:11 -0700

    add text score display to pong

--- src/02-pong/Ball.cpp
+++ src/02-pong/Ball.cpp
@@ -1,5 +1,4 @@
 #include "Ball.h"
-#include <imgui.h>
 
 Ball::Ball(int left, int top, int right, int bottom) :
     m_left(left), m_top(top), m_right(right), m_bottom(bottom),
@@ -11,38 +10,39 @@ void Ball::update(float deltaTime)
 {
     const auto motion = mathf::trajectory(mathf::toRadians(m_direction), m_speed * deltaTime);
 
-    if (ImGui::Begin("Ball Info"))
-    {
-        ImGui::Text("%s", format("Angle: {}, Movement: {}", m_direction, motion).c_str());
-    }
-    ImGui::End();
-
     m_position += motion;
 
     // simple algorithm, but potentially error prone with pass through collisions if speed > width
 
     // reflect top/bottom
-    if (m_position.y <= m_top)
+    if (m_position.y < m_top)
     {
         m_direction += (180.f - m_direction + 180.f) * 2.f;
+        m_position.y -= (m_position.y - m_top) * 2.f;
     }
-    else if (m_position.y >= m_bottom - BallWidth)
+    else if (m_position.y > m_bottom - BallWidth)
     {
         m_direction += (-m_direction + 180.f) * 2.f;
+        m_position.y -= (m_position.y - (m_bottom - BallWidth)) * 2.f;
     }
 
     // left / right : TODO: remove bounce behavior
-    if (m_position.x <= m_left)
+    if (m_position.x < m_left)
     {
         m_direction += (90.f - m_direction + 180.f) * 2.f;
+        m_position.x -= (m_position.x - m_left) * 2.f;
         onScore(Paddle::PaddleId::Computer);
     }
-    else if (m_position.x >= m_right - BallWidth)
+    else if (m_position.x > m_right - BallWidth)
     {
         m_direction += (270.f - m_direction + 180.f) * 2.f;
+        m_position.x -= (m_position.x - (m_right - BallWidth)) * 2.f;
         onScore(Paddle::PaddleId::Player);
     }
 
+    // constrain values
+    m_position.x = mathf::clamp(m_position.x, m_left, m_right - BallWidth);
+    m_position.y = mathf::clamp(m_position.y, m_top, m_bottom - BallWidth);
     m_direction = mathf::wrap(m_direction, 0, 360);
 }
 
--- src/02-pong/Pong.h
+++ src/02-pong/Pong.h
@@ -1,9 +1,12 @@
 #pragma once
 #include <sdgl/sdgl.h>
+
 #include "SpriteBatch.h"
 
 #include "Paddle.h"
 #include "Ball.h"
+
+#include <imgui.h>
 
 using namespace sdgl;
 
@@ -16,26 +19,22 @@ protected:
     SpriteBatch batch{};
     Paddle *player{}, *computer{};
     Ball *ball{};
+    BitmapFont font;
     Camera2D cam{};
 
-    bool init() override
+    int score[2] = {0};
+    FontText scoreText[2];
+
+    void reset()
     {
-        batch.init();
-        Paddle::provideInput(window()->input());
-        player = new Paddle(32, 256, Color::White, Paddle::Player);
-        computer = new Paddle(32, 256, Color::White, Paddle::Computer);
+        score[0] = 1000;
+        score[1] = 1234;
 
         int fw, fh;
         window()->getFrameBufferSize(&fw, &fh);
-        cam.setViewport({0, 0, fw, fh});
-        cam.setOrigin({0, 0});
-        ball = new Ball(0, 0, fw, fh);
         ball->position({(float)fw/2.f, (float)fh/2.f});
         ball->direction(mathf::rand(360.f));
-
-        ball->onScore += [](Paddle::PaddleId id) {
-            DEBUG_LOG("Player {} scored!", (int)id);
-        };
+        ball->speed(400.f);
 
         player->position({
             8,
@@ -46,6 +45,46 @@ protected:
             static_cast<float>(fh) / 2.f - 128.f
         });
 
+        scoreText[0].font(&font);
+        scoreText[1].font(&font);
+        scoreText[0].setText(std::to_string(score[0]));
+        scoreText[1].setText(std::to_string(score[1]));
+    }
+
+    void handleScore(Paddle::PaddleId id)
+    {
+        ++score[id];
+        scoreText[id].setText(std::to_string(score[id]));
+    }
+
+    bool init() override
+    {
+        batch.init();
+        Paddle::provideInput(window()->input());
+
+        // load assets
+        SDGL_ASSERT(font.loadBMFont("assets/pong.fnt"));
+
+        // create paddles
+        player = new Paddle(32, 256, Color::White, Paddle::Player);
+        computer = new Paddle(32, 256, Color::White, Paddle::Computer);
+
+        int fw, fh;
+        window()->getFrameBufferSize(&fw, &fh);
+
+        // init camera
+        cam.setViewport({0, 0, fw, fh});
+        cam.setOrigin({0, 0});
+
+        // create ball
+        ball = new Ball(0, 0, fw, fh);
+        ball->position({(float)fw/2.f, (float)fh/2.f});
+        ball->direction(mathf::rand(360.f));
+        ball->speed(1600.f);
+
+        ball->onScore += Callback(this, &Pong::handleScore);
+
+        reset();
         return true;
     }
 
@@ -61,10 +100,17 @@ protected:
     {
         window()->clear({20, 20, 20, 255});
 
+        int w, h;
+        window()->getFrameBufferSize(&w, &h);
+
         batch.begin(cam.getMatrix());
             player->draw(&batch);
             computer->draw(&batch);
             ball->draw(&batch);
+
+            batch.drawText(scoreText[0], {16.f, 16.f});
+            batch.drawText(scoreText[1], {(float)w - scoreText[1].currentSize().x - 16.f, 16.f});
+
         batch.end();
     }
 
